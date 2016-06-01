f0f2eec7916ecbf5ee7f781edc011752234310da
Jake Bell <jakebrbell@gmail.com>
2016-06-01 13:24:36 -0700

    Add functionality to google map

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 // Google Maps API key: AIzaSyClyM6WEEfq2z5cOzBtedaQfia7FFjam1U
 
 var map;
+var camp14Marker;
+var camp15Marker;
+var camp16Marker;
+var camp25Marker;
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 40.3, lng: 127.8},
@@ -9,12 +14,6 @@ function initMap() {
     zoomControl: true,
     scrollwheel: false,
     draggable: true
-  });
-
-  var marker = new google.maps.Marker({
-    position: {lat: 39.3415, lng: 126.0319},
-    map: map,
-    title: 'Camp No. 14'
   });
 
   var styles = [
@@ -39,6 +38,27 @@ function initMap() {
     }
   ];
 
+  camp14Marker = new google.maps.Marker({
+    position: {lat: 39.3415, lng: 126.0319},
+    // animation: google.maps.Animation.DROP,
+    title: 'Camp No. 14'
+  });
+  camp15Marker = new google.maps.Marker({
+    position: {lat: 39.4032, lng: 126.5059},
+    // animation: google.maps.Animation.DROP,
+    title: 'Camp No. 15'
+  });
+  camp16Marker = new google.maps.Marker({
+    position: {lat: 41.1849, lng: 129.2032},
+    // animation: google.maps.Animation.DROP,
+    title: 'Camp No. 16'
+  });
+  camp25Marker = new google.maps.Marker({
+    position: {lat: 41.5002, lng: 129.4334},
+    // animation: google.maps.Animation.DROP,
+    title: 'Camp No. 25'
+  });
+
   map.setOptions({styles: styles});
 }
 
@@ -46,15 +66,57 @@ function initMap() {
 
   $('.scrollspy').scrollSpy();
 
+  var changeContent = function() {
+    $('.facts').fadeOut().fadeIn();
+    $('.testimonials').fadeOut().fadeIn();
+    $('.satellite-photo').fadeOut().fadeIn();
+  };
 
+  $('.camp-buttons').on('click', function(event) {
+    var $target = $(event.target);
 
-  // var map;
-  // function initMap() {
-  //   map = new google.maps.Map(document.getElementById('map'), {
-  //     center: {lat: -34.397, lng: 150.644},
-  //     zoom: 8
-  //   });
-  // }
+    if ($target.text().trim() === 'Camp Number 14') {
+      camp14Marker.setMap(map);
+      camp15Marker.setMap(null);
+      camp16Marker.setMap(null);
+      camp25Marker.setMap(null);
+
+      $('.camp-name').removeClass('active-button');
+      $target.addClass('active-button');
+      changeContent();
+    }
+    else if ($target.text().trim() === 'Camp Number 15') {
+      camp14Marker.setMap(null);
+      camp15Marker.setMap(map);
+      camp16Marker.setMap(null);
+      camp25Marker.setMap(null);
+
+      $('.camp-name').removeClass('active-button');
+      $target.addClass('active-button');
+      changeContent();
+    }
+    else if ($target.text().trim() === 'Camp Number 16') {
+      camp14Marker.setMap(null);
+      camp15Marker.setMap(null);
+      camp16Marker.setMap(map);
+      camp25Marker.setMap(null);
+
+      $('.camp-name').removeClass('active-button');
+      $target.addClass('active-button');
+      changeContent();
+    }
+    else if ($target.text().trim() === 'Camp Number 25') {
+      camp14Marker.setMap(null);
+      camp15Marker.setMap(null);
+      camp16Marker.setMap(null);
+      camp25Marker.setMap(map);
+
+      $('.camp-name').removeClass('active-button');
+      $target.addClass('active-button');
+      changeContent();
+    }
+  });
+
 
   // var $xhr = $.getJSON('https://aqk5q11wx9.execute-api.us-east-1.amazonaws.com/test');
   //
