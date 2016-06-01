f1df7cc40c1a0995ccec9eadbca6cabc9d4ef7f9
Jake Bell <jakebrbell@gmail.com>
2016-06-01 10:03:11 -0700

    Close #4 Fix google map

--- js/app.js
+++ js/app.js
@@ -1,9 +1,53 @@
 // Google Maps API key: AIzaSyClyM6WEEfq2z5cOzBtedaQfia7FFjam1U
 
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.3, lng: 127.8},
+    zoom: 6,
+    disableDefaultUI: true,
+    zoomControl: true,
+    scrollwheel: false,
+    draggable: true
+  });
+
+  var marker = new google.maps.Marker({
+    position: {lat: 39.3415, lng: 126.0319},
+    map: map,
+    title: 'Camp No. 14'
+  });
+
+  var styles = [
+    {
+      stylers: [
+        { hue: "#00ffe6" },
+        { saturation: -20 }
+      ]
+    },{
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        { lightness: 100 },
+        { visibility: "simplified" }
+      ]
+    },{
+      featureType: "road",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]
+    }
+  ];
+
+  map.setOptions({styles: styles});
+}
+
 (function() {
 
   $('.scrollspy').scrollSpy();
-  
+
+
+
   // var map;
   // function initMap() {
   //   map = new google.maps.Map(document.getElementById('map'), {
